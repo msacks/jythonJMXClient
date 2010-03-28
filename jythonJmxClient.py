769b9cb34b734acdb8eeae5b1c913fc91c4f2cd6
Matthew Sacks <matthew@matthewsacks.com>
2010-03-28 11:43:27 -0700

    added cmd and option parsing. added tests for socket connection.

--- jythonJmxClient.py
+++ jythonJmxClient.py
@@ -1,31 +1,80 @@
-# Jython Script for gathering metrics via JMX
-# matthew@matthewsacks.com
+# jythonJmxClient.py
+# Jython Script for gathering metrics via JMX via command line interface
+# matthew@thebitsource.com
 # Licensed under the Apache License (AL2)
+# v 0.2.0
 
+#Java Dependencies
 import javax.management.remote.JMXConnector;
 import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 import java.lang.management.ManagementFactory;
 
-import sys,cmd
+#Python Dependencies
+import sys, cmd, socket, optparse
 from urlparse import urljoin
+from cmd import Cmd
 
-#TODO: Replace sys with cmd option parsing
+class jmxCmd(Cmd):
+	prompt = 'jymx> '
 
-host = sys.argv[1]
-port = sys.argv[2]
+	def do_connect(self, line):
+		#annoying option parsing stuff
+		parser = optparse.OptionParser(conflict_handler="resolve")
+		parser.add_option('-h', '--hostip', dest='host')
+		parser.add_option('-p', '--portnum', dest='port')
+		(options, args) = parser.parse_args(line.split())
+	
+		options.port = int(options.port)
 
-serviceURL = "service:jmx:rmi:///jndi/rmi://"
-serviceURL = serviceURL + host + ":" + port + "/jmxrmi"
+		print "Host: ", type(options.host)
+		print "Port: ", type(options.port)
+		
+		connectionArgs = ((options.host,options.port))
+		
+		#Test Connection Arguments
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+			s.bind(connectionArgs)
+			s.close()
+		except socket.error, (value,message):
+			if s:
+				s.close()
+			print "Could not open connection: " + message
+			sys.exit(1)
 
-url =  javax.management.remote.JMXServiceURL(serviceURL);
+		#Establish Connection to JMX Server
+		serviceURL = str()
+		serviceURL = "service:jmx:rmi:///jndi/rmi://"
+		serviceURL = serviceURL + options.host + ":" + str(options.port) + "/jmxrmi"
+		
+		#DEBUG
+		print options.host, options.port, serviceURL
 
-connector = javax.management.remote.JMXConnectorFactory.connect(url);
-remote = connector.getMBeanServerConnection();
+		url =  javax.management.remote.JMXServiceURL(serviceURL);
+		connector = javax.management.remote.JMXConnectorFactory.connect(url);
+		global remote
+		remote = connector.getMBeanServerConnection();
+		
+	def do_getDomains(self, line):
+		domainList = []
+		domainList = remote.getDomains()
+		
+		for element in domainList:
+			print element
 
-obn =  javax.management.ObjectName("java.lang:type=Memory");
-#remoteRuntime = java.lang.management.ManagementFactory.newPlatformMXBeanProxy(remote,ManagementFactory.RUNTIME_MXBEAN_NAME,RuntimeMXBean.class);
-result = remote.getAttribute(obn,"HeapMemoryUsage");
+	#def do_getAttribute(self, line):
+		
+	#	obn =  javax.management.ObjectName("java.lang:type=Memory");
+	#	result = remote.getAttribute(obn,"HeapMemoryUsage");
 
-print result
+	def do_quit(self, arg):
+        	print("bye.")
+       		return True    
+	
+	default_to_shell = True
 
+
+if __name__ == '__main__':
+	jmxCmd().cmdloop()		
