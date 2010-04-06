2eb036d20c05f4ac3277c53afb046da5bc0947a6
Matthew Sacks <msacks@fermat.local>
2010-04-05 20:46:35 -0700

    added help messages and removed debug statements

--- jythonJmxClient.py
+++ jythonJmxClient.py
@@ -2,8 +2,8 @@
 # Jython Script for gathering metrics via JMX via command line interface
 # matthew@thebitsource.com
 # Licensed under the Apache License (AL2)
-# v 0.2.0
-#
+# v 0.2.1
+# NOTE: Commands may also be passed as a script, just store commands as a text file and feed it to the parser
 
 #Java Dependencies
 import javax.management.remote.JMXConnector;
@@ -17,9 +17,12 @@ from urlparse import urljoin
 from cmd import Cmd
 
 class jmxCmd(Cmd):
-	prompt = 'jymx> '
+	prompt = 'jmx> '
 
 	def do_connect(self, line):
+                """connect -h|--hostip <hostname or ip_addr> -p|--portnum <port>
+                Establish a connection to the JMX Server. Uses jmxrmi protocol by default"""
+		
 		#annoying option parsing stuff
 		parser = optparse.OptionParser(conflict_handler="resolve")
 		parser.add_option('-h', '--hostip', dest='host')
@@ -27,9 +30,6 @@ class jmxCmd(Cmd):
 		(options, args) = parser.parse_args(line.split())
 	
 		options.port = int(options.port)
-
-		print "Host: ", type(options.host)
-		print "Port: ", type(options.port)
 		
 		connectionArgs = ((options.host,options.port))
 		
@@ -50,32 +50,41 @@ class jmxCmd(Cmd):
 		serviceURL = "service:jmx:rmi:///jndi/rmi://"
 		serviceURL = serviceURL + options.host + ":" + str(options.port) + "/jmxrmi"
 		
-		#DEBUG
-		print options.host, options.port, serviceURL
-
 		url =  javax.management.remote.JMXServiceURL(serviceURL);
 		connector = javax.management.remote.JMXConnectorFactory.connect(url);
 		global remote
 		remote = connector.getMBeanServerConnection();
 		
 	def do_getDomains(self, line):
+                """getDomains 
+                Retrieve a list of all available JMX Domains"""
+
 		domainList = []
 		domainList = remote.getDomains()
 		
 		for element in domainList:
 			print element
 
-	#def do_getAttribute(self, line):
+	def do_getAttribute(self, line):
+                """getAttribute -m|--mbeanpath <path to mbeanr> -a|--attribute <name of attribute>
+                Query the mbean server for a specific attribute and return the result"""
+
+		#annoying option parsing stuff - need cmd2
+		parser = optparse.OptionParser(conflict_handler="error")
+		parser.add_option('-m', '--mbeanpath', dest='mbp', type='string')
+		parser.add_option('-a', '--attribute', dest='attr')
+		(options, args) = parser.parse_args(line.split())
 		
-	#	obn =  javax.management.ObjectName("java.lang:type=Memory");
-	#	result = remote.getAttribute(obn,"HeapMemoryUsage");
+		obn =  javax.management.ObjectName(options.mbp);
+		result = remote.getAttribute(obn, options.attr);
+
+		print result		
 
 	def do_quit(self, arg):
         	print("bye.")
        		return True    
 	
 	default_to_shell = True
-
 
 if __name__ == '__main__':
 	jmxCmd().cmdloop()		
